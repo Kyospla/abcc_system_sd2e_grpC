bb33d0aa87d27c2ab0616617eb700cc82187ead1
tsuruda00 <115763734+tsuruda00@users.noreply.github.com>
2022-12-13 15:05:14 +0900

    詳細修正

--- DBManager.php
+++ DBManager.php
@@ -66,4 +66,37 @@ class DBManager
     $result = $ps->fetchAll();
     return $result;
   }
+  //商品名を取得
+  public function getProduct_name()
+  {
+    $pdo = $this->dbConnect();
+    $sql = "SELECT product_name FROM product_tbl";
+    $ps = $pdo->query($sql);
+    $ps->execute();
+
+    $result = $ps->fetchAll();
+    return $result;
+  }
+  //商品価格を取得
+  public function getProduct_price()
+  {
+    $pdo = $this->dbConnect();
+    $sql = "SELECT product_price FROM product_tbl";
+    $ps = $pdo->query($sql);
+    $ps->execute();
+
+    $result = $ps->fetchAll();
+    return $result;
+  }
+  //商品IDを取得
+  public function getProduct_id()
+  {
+    $pdo = $this->dbConnect();
+    $sql = "SELECT product_id FROM product_tbl";
+    $ps = $pdo->query($sql);
+    $ps->execute();
+
+    $result = $ps->fetchAll();
+    return $result;
+  }
 }
